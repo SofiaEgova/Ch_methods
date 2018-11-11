8e224a56a762405bda43a9d09a872ffa5724ecb7
Sofia <sofiakulymanova@gmail.com>
2018-11-12 02:20:01 +0400

    начало(1-2?)

---
+++ Labs/Labs/FormMain.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Labs
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxa = new System.Windows.Forms.TextBox();
+            this.textBoxb = new System.Windows.Forms.TextBox();
+            this.textBoxc = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxZ = new System.Windows.Forms.TextBox();
+            this.textBoxMain = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(64, 165);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(149, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Рассчитать";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Enabled = false;
+            this.textBoxX.Location = new System.Drawing.Point(64, 9);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(149, 22);
+            this.textBoxX.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "a";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "b";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "c";
+            // 
+            // textBoxa
+            // 
+            this.textBoxa.Enabled = false;
+            this.textBoxa.Location = new System.Drawing.Point(64, 37);
+            this.textBoxa.Name = "textBoxa";
+            this.textBoxa.Size = new System.Drawing.Size(149, 22);
+            this.textBoxa.TabIndex = 6;
+            // 
+            // textBoxb
+            // 
+            this.textBoxb.Enabled = false;
+            this.textBoxb.Location = new System.Drawing.Point(64, 65);
+            this.textBoxb.Name = "textBoxb";
+            this.textBoxb.Size = new System.Drawing.Size(149, 22);
+            this.textBoxb.TabIndex = 7;
+            // 
+            // textBoxc
+            // 
+            this.textBoxc.Enabled = false;
+            this.textBoxc.Location = new System.Drawing.Point(64, 93);
+            this.textBoxc.Name = "textBoxc";
+            this.textBoxc.Size = new System.Drawing.Size(149, 22);
+            this.textBoxc.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Z";
+            // 
+            // textBoxZ
+            // 
+            this.textBoxZ.Enabled = false;
+            this.textBoxZ.Location = new System.Drawing.Point(64, 125);
+            this.textBoxZ.Name = "textBoxZ";
+            this.textBoxZ.Size = new System.Drawing.Size(149, 22);
+            this.textBoxZ.TabIndex = 10;
+            // 
+            // textBoxMain
+            // 
+            this.textBoxMain.Enabled = false;
+            this.textBoxMain.Location = new System.Drawing.Point(234, 9);
+            this.textBoxMain.Multiline = true;
+            this.textBoxMain.Name = "textBoxMain";
+            this.textBoxMain.Size = new System.Drawing.Size(665, 308);
+            this.textBoxMain.TabIndex = 11;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 330);
+            this.Controls.Add(this.textBoxMain);
+            this.Controls.Add(this.textBoxZ);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxc);
+            this.Controls.Add(this.textBoxb);
+            this.Controls.Add(this.textBoxa);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "FormMain";
+            this.Text = "Главная";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxa;
+        private System.Windows.Forms.TextBox textBoxb;
+        private System.Windows.Forms.TextBox textBoxc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxZ;
+        private System.Windows.Forms.TextBox textBoxMain;
+    }
+}
+
