4bdf792f1d4545123abe1b545cfd42205b5b2f67
Sofia <sofiakulymanova@gmail.com>
2018-11-25 23:50:15 +0400

    лаба 2. полином Лагранжа

---
+++ Labs/Labs/FormL2.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Labs
+{
+    public partial class FormL2 : Form
+    {
+        float x0;
+        float x1;
+        float x2;
+        float x3;
+        float x4;
+        float y0;
+        float y1;
+        float y2;
+        float y3;
+        float y4;
+        float P0;
+        float P1;
+        float P2;
+        float P3;
+        float P4;
+        float k0;
+        float k1;
+        float k2;
+        float k3;
+        float k4;
+        string res = "";
+        Graphics g2;
+
+        public FormL2()
+        {
+            InitializeComponent();
+            textBoxX0.Text = "0,083";
+            textBoxX1.Text = "0,472";
+            textBoxX2.Text = "1,347";
+            textBoxX3.Text = "2,117";
+            textBoxX4.Text = "2,947";
+            textBoxY0.Text = "-2,132";
+            textBoxY1.Text = "-2,013";
+            textBoxY2.Text = "-1,613";
+            textBoxY3.Text = "-0,842";
+            textBoxY4.Text = "2,973";
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            res = "";
+            g2 = panelG1.CreateGraphics();
+            g2.Clear(panelG1.BackColor);
+            x0 = float.Parse(textBoxX0.Text);
+            x1 = float.Parse(textBoxX1.Text);
+            x2 = float.Parse(textBoxX2.Text);
+            x3 = float.Parse(textBoxX3.Text);
+            x4 = float.Parse(textBoxX4.Text);
+            y0 = float.Parse(textBoxY0.Text);
+            y1 = float.Parse(textBoxY1.Text);
+            y2 = float.Parse(textBoxY2.Text);
+            y3 = float.Parse(textBoxY3.Text);
+            y4 = float.Parse(textBoxY4.Text);
+
+            //полином Лангранжа
+            CalcPolinom();
+            CalcKoefLangr();
+            StringPolinom();
+            textBoxR1.Text = res;
+            DrawPolinom();
+        }
+
+        private void CalcPolinom()
+        {
+            P0 = 1 / ((x0 - x1) * (x0 - x2) * (x0 - x3) * (x0 - x4));
+            P1 = 1 / ((x1 - x0) * (x1 - x2) * (x1 - x3) * (x1 - x4));
+            P2 = 1 / ((x2 - x0) * (x2 - x1) * (x2 - x3) * (x2 - x4));
+            P3 = 1 / ((x3 - x0) * (x3 - x1) * (x3 - x2) * (x3 - x4));
+            P4 = 1 / ((x4 - x0) * (x4 - x1) * (x4 - x2) * (x4 - x3));
+            res += "P0 = " + P0 + "P1 = " + P1 + "P2 = " + P2 + "P3 = " + P3 + "P4 = " + P4+"\r\n";
+        }
+
+        private void CalcKoefLangr()
+        {
+            k0 = P0 * y0;
+            k1 = P1 * y1;
+            k2 = P2 * y2;
+            k3 = P3 * y3;
+            k4 = P4 * y4;
+            res += "k0 = " + k0 + "k1 = " + k1 + "k2 = " + k2 + "k3 = " + k3 + "k4 = " + k4 + "\r\n\r\n";
+        }
+
+        private void StringPolinom()
+        {
+            res += P0 * y0 + "*(x-( " + x1 + "))*(x-(" + x2 + "))*(x-(" + x3 + "))*(x-(" + x4 + "))\r\n" +
+            P1 * y1 + "*(x-( " + x0 + "))*(x-(" + x2 + "))*(x-(" + x3 + "))*(x-(" + x4 + "))\r\n" +
+            P2 * y2 + "*(x-( " + x0 + "))*(x-(" + x1 + "))*(x-(" + x3 + "))*(x-(" + x4 + "))\r\n" +
+            P3 * y3 + "*(x-( " + x0 + "))*(x-(" + x1 + "))*(x-(" + x2 + "))*(x-(" + x4 + "))\r\n" +
+            P4 * y4 + "*(x-( " + x0 + "))*(x-(" + x1 + "))*(x-(" + x2 + "))*(x-(" + x3 + "))";
+        }
+
+        private void DrawPolinom()
+        {
+            g2.TranslateTransform(panelG1.Width / 2, panelG1.Height / 2);
+            g2.ScaleTransform(1, -1);
+            Pen myPen1 = new Pen(Color.Blue);
+            g2.DrawLine(myPen1, 0, g2.Transform.OffsetY, 0, -g2.Transform.OffsetY);
+            g2.DrawLine(myPen1, 0, 0, g2.Transform.OffsetX, 0);
+            Pen myPen2 = new Pen(Color.Red);
+            float h2 = float.Parse(0.1 + "");
+            for (float i = 0; i < 5; i += h2)
+            {
+                float y = k0 * (i - x1) * (i - x2) * (i - x3) * (i - x4) +
+                k1 * (i - x0) * (i - x2) * (i - x3) * (i - x4) +
+                k2 * (i - x0) * (i - x1) * (i - x3) * (i - x4) +
+                k3 * (i - x0) * (i - x1) * (i - x2) * (i - x4) +
+                k4 * (i - x0) * (i - x1) * (i - x2) * (i - x2);
+                float yN = k0 * (i + h2 - x1) * (i + h2 - x2) * (i + h2 - x3) * (i + h2 - x4) +
+                k1 * (i + h2 - x0) * (i + h2 - x2) * (i + h2 - x3) * (i + h2 - x4) +
+                k2 * (i + h2 - x0) * (i + h2 - x1) * (i + h2 - x3) * (i + h2 - x4) +
+                k3 * (i + h2 - x0) * (i + h2 - x1) * (i + h2 - x2) * (i + h2 - x4) +
+                k4 * (i + h2 - x0) * (i + h2 - x1) * (i + h2 - x2) * (i + h2 - x2);
+                g2.DrawLine(myPen2, float.Parse(i.ToString()) * 20, (y) * 10, (i + h2) * 20, (yN) * 10);
+            }
+        }
+    }
+}
---
+++ Labs/Labs/FormL2.designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Labs
+{
+    partial class FormL2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxX0 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.panelG1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxX1 = new System.Windows.Forms.TextBox();
+            this.textBoxX2 = new System.Windows.Forms.TextBox();
+            this.textBoxX3 = new System.Windows.Forms.TextBox();
+            this.textBoxX4 = new System.Windows.Forms.TextBox();
+            this.textBoxY0 = new System.Windows.Forms.TextBox();
+            this.textBoxY1 = new System.Windows.Forms.TextBox();
+            this.textBoxY2 = new System.Windows.Forms.TextBox();
+            this.textBoxY3 = new System.Windows.Forms.TextBox();
+            this.textBoxY4 = new System.Windows.Forms.TextBox();
+            this.textBoxR1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxX0
+            // 
+            this.textBoxX0.Location = new System.Drawing.Point(56, 12);
+            this.textBoxX0.Name = "textBoxX0";
+            this.textBoxX0.Size = new System.Drawing.Size(100, 22);
+            this.textBoxX0.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(628, 11);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(137, 59);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // panelG1
+            // 
+            this.panelG1.Location = new System.Drawing.Point(15, 278);
+            this.panelG1.Name = "panelG1";
+            this.panelG1.Size = new System.Drawing.Size(565, 206);
+            this.panelG1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Y";
+            // 
+            // textBoxX1
+            // 
+            this.textBoxX1.Location = new System.Drawing.Point(162, 12);
+            this.textBoxX1.Name = "textBoxX1";
+            this.textBoxX1.Size = new System.Drawing.Size(100, 22);
+            this.textBoxX1.TabIndex = 5;
+            // 
+            // textBoxX2
+            // 
+            this.textBoxX2.Location = new System.Drawing.Point(268, 12);
+            this.textBoxX2.Name = "textBoxX2";
+            this.textBoxX2.Size = new System.Drawing.Size(100, 22);
+            this.textBoxX2.TabIndex = 6;
+            // 
+            // textBoxX3
+            // 
+            this.textBoxX3.Location = new System.Drawing.Point(374, 12);
+            this.textBoxX3.Name = "textBoxX3";
+            this.textBoxX3.Size = new System.Drawing.Size(100, 22);
+            this.textBoxX3.TabIndex = 7;
+            // 
+            // textBoxX4
+            // 
+            this.textBoxX4.Location = new System.Drawing.Point(480, 12);
+            this.textBoxX4.Name = "textBoxX4";
+            this.textBoxX4.Size = new System.Drawing.Size(100, 22);
+            this.textBoxX4.TabIndex = 8;
+            // 
+            // textBoxY0
+            // 
+            this.textBoxY0.Location = new System.Drawing.Point(56, 48);
+            this.textBoxY0.Name = "textBoxY0";
+            this.textBoxY0.Size = new System.Drawing.Size(100, 22);
+            this.textBoxY0.TabIndex = 9;
+            // 
+            // textBoxY1
+            // 
+            this.textBoxY1.Location = new System.Drawing.Point(162, 48);
+            this.textBoxY1.Name = "textBoxY1";
+            this.textBoxY1.Size = new System.Drawing.Size(100, 22);
+            this.textBoxY1.TabIndex = 10;
+            // 
+            // textBoxY2
+            // 
+            this.textBoxY2.Location = new System.Drawing.Point(268, 48);
+            this.textBoxY2.Name = "textBoxY2";
+            this.textBoxY2.Size = new System.Drawing.Size(100, 22);
+            this.textBoxY2.TabIndex = 11;
+            // 
+            // textBoxY3
+            // 
+            this.textBoxY3.Location = new System.Drawing.Point(374, 48);
+            this.textBoxY3.Name = "textBoxY3";
+            this.textBoxY3.Size = new System.Drawing.Size(100, 22);
+            this.textBoxY3.TabIndex = 12;
+            // 
+            // textBoxY4
+            // 
+            this.textBoxY4.Location = new System.Drawing.Point(480, 48);
+            this.textBoxY4.Name = "textBoxY4";
+            this.textBoxY4.Size = new System.Drawing.Size(100, 22);
+            this.textBoxY4.TabIndex = 13;
+            // 
+            // textBoxR1
+            // 
+            this.textBoxR1.Location = new System.Drawing.Point(15, 93);
+            this.textBoxR1.Multiline = true;
+            this.textBoxR1.Name = "textBoxR1";
+            this.textBoxR1.Size = new System.Drawing.Size(565, 169);
+            this.textBoxR1.TabIndex = 14;
+            // 
+            // FormL2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 511);
+            this.Controls.Add(this.textBoxR1);
+            this.Controls.Add(this.textBoxY4);
+            this.Controls.Add(this.textBoxY3);
+            this.Controls.Add(this.textBoxY2);
+            this.Controls.Add(this.textBoxY1);
+            this.Controls.Add(this.textBoxY0);
+            this.Controls.Add(this.textBoxX4);
+            this.Controls.Add(this.textBoxX3);
+            this.Controls.Add(this.textBoxX2);
+            this.Controls.Add(this.textBoxX1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panelG1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxX0);
+            this.Name = "FormL2";
+            this.Text = "Полином Лагранжа";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxX0;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Panel panelG1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxX1;
+        private System.Windows.Forms.TextBox textBoxX2;
+        private System.Windows.Forms.TextBox textBoxX3;
+        private System.Windows.Forms.TextBox textBoxX4;
+        private System.Windows.Forms.TextBox textBoxY0;
+        private System.Windows.Forms.TextBox textBoxY1;
+        private System.Windows.Forms.TextBox textBoxY2;
+        private System.Windows.Forms.TextBox textBoxY3;
+        private System.Windows.Forms.TextBox textBoxY4;
+        private System.Windows.Forms.TextBox textBoxR1;
+    }
+}
