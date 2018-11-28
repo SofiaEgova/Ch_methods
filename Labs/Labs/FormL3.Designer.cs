b3f73dab24e29d814f60cd0de5b832cab0d9d095
Sofia <sofiakulymanova@gmail.com>
2018-11-28 11:56:46 +0400

    лаба3. ф. трапеций и ф. симпсона

---
+++ Labs/Labs/FormL3.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Labs
+{
+    partial class FormL3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxF = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxa = new System.Windows.Forms.TextBox();
+            this.textBoxb = new System.Windows.Forms.TextBox();
+            this.textBoxRes = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 101);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Функция:";
+            // 
+            // textBoxF
+            // 
+            this.textBoxF.Location = new System.Drawing.Point(89, 14);
+            this.textBoxF.Name = "textBoxF";
+            this.textBoxF.Size = new System.Drawing.Size(100, 22);
+            this.textBoxF.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "a=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "b=";
+            // 
+            // textBoxa
+            // 
+            this.textBoxa.Location = new System.Drawing.Point(89, 44);
+            this.textBoxa.Name = "textBoxa";
+            this.textBoxa.Size = new System.Drawing.Size(100, 22);
+            this.textBoxa.TabIndex = 5;
+            // 
+            // textBoxb
+            // 
+            this.textBoxb.Location = new System.Drawing.Point(89, 72);
+            this.textBoxb.Name = "textBoxb";
+            this.textBoxb.Size = new System.Drawing.Size(100, 22);
+            this.textBoxb.TabIndex = 6;
+            // 
+            // textBoxRes
+            // 
+            this.textBoxRes.Location = new System.Drawing.Point(15, 130);
+            this.textBoxRes.Multiline = true;
+            this.textBoxRes.Name = "textBoxRes";
+            this.textBoxRes.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxRes.Size = new System.Drawing.Size(985, 290);
+            this.textBoxRes.TabIndex = 7;
+            // 
+            // FormL3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 513);
+            this.Controls.Add(this.textBoxRes);
+            this.Controls.Add(this.textBoxb);
+            this.Controls.Add(this.textBoxa);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxF);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "FormL3";
+            this.Text = "FormL3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxF;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxa;
+        private System.Windows.Forms.TextBox textBoxb;
+        private System.Windows.Forms.TextBox textBoxRes;
+    }
+}
---
+++ Labs/Labs/FormL3.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labs
+{
+    public partial class FormL3 : Form
+    {
+        string formula = "Sqrt(x)*Log(x)";
+        int n = 36;
+        double h = 0.084;
+        int a = 1;
+        int b = 4;
+        string resultat = "";
+        double temp = 0.0;
+
+        double[] xh;
+        double[] fxh;
+        double[] x2h;
+        double[] fx2h;
+        double tZnachH;
+        double tZnach2H;
+        double sZnachH;
+        double sZnach2H;
+
+        public FormL3()
+        {
+            InitializeComponent();
+            textBoxF.Text = formula;
+            textBoxa.Text = a + "";
+            textBoxb.Text = b + "";
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                formula = textBoxF.Text;
+                a = Convert.ToInt32(textBoxa.Text);
+                b = Convert.ToInt32(textBoxb.Text);
+            }
+            catch (Exception ex)
+            {
+                textBoxRes.Text = "Данные введены не правильно! :( \r\n" + ex;
+                return;
+            }
+
+            trapeciy();
+            simpson();
+            textBoxRes.Text = resultat;
+        }
+
+        private void trapeciy()
+        {
+            resultat += "По формуле трапеций:\r\n";
+            xh = new double[n];
+            xh[0] = a;
+            resultat += "С шагом h: \r\n x = { ";
+            for (int i = 1; i < xh.Length; i++)
+            {
+                xh[i] = xh[i - 1] + h;
+                resultat += xh[i] + " , ";
+            }
+            resultat += "} \r\n\r\n";
+
+            resultat += " f(x) = { ";
+            fxh = new double[n];
+            for (int i = 0; i < fxh.Length; i++)
+            {
+                mathAsync(form(xh[i], formula));
+                fxh[i] = temp;
+                resultat += fxh[i] + " , ";
+            }
+            resultat += "} \r\n\r\n";
+
+            //значение h
+            double sumh = 0;
+            for (int i = 1; i < fxh.Length - 2; i++)
+            {
+                sumh += fxh[i];
+            }
+            tZnachH = h * ((fxh[0] + fxh[n - 1]) / 2 + sumh);
+            resultat += "I(h) = " + tZnachH + "\r\n\r\n";
+
+            x2h = new double[n / 2];
+            x2h[0] = a;
+            resultat += "С шагом 2h: \r\n x = { ";
+            for (int i = 1; i < x2h.Length; i++)
+            {
+                x2h[i] = x2h[i - 1] + h * 2;
+                resultat += x2h[i] + " , ";
+            }
+            resultat += "} \r\n\r\n";
+
+            resultat += " f(x) = { ";
+            fx2h = new double[n / 2];
+            for (int i = 0; i < fx2h.Length; i++)
+            {
+                mathAsync(form(x2h[i], formula));
+                fx2h[i] = temp;
+                resultat += fx2h[i] + " , ";
+            }
+            resultat += "} \r\n\r\n";
+
+            //значение 2h
+            double sum2h = 0;
+            for (int i = 1; i < fx2h.Length - 2; i++)
+            {
+                sum2h += fx2h[i];
+            }
+            tZnach2H = h * 2 * ((fx2h[0] + fx2h[n / 2 - 1]) / 2 + sum2h);
+            resultat += "I(2h) = " + tZnach2H + "\r\n\r\n";
+
+            resultat += "I(h) - I(2h) = " + (tZnachH - tZnach2H) / 3 + "\r\n";
+            resultat += "Значение интеграла = " + tZnachH + " +- " + (tZnachH - tZnach2H) / 3 + "\r\n\r\n";
+        }
+
+        private void simpson()
+        {
+            resultat += "По формуле Симпсона:\r\n";
+            resultat += " чет/нечет h = { ";
+            double[] chetNechH = new double[n - 2];
+            for(int i=0;i< chetNechH.Length; i++)
+            {
+                if ((i + 1) % 2 == 0)
+                    chetNechH[i] = fxh[i + 1] * 2;
+                else chetNechH[i] = fxh[i + 1] * 4;
+                resultat += chetNechH[i] + " , ";
+            }
+            resultat += "} \r\n\r\n";
+
+            resultat += " чет/нечет 2h = { ";
+            double[] chetNech2H = new double[n/2 - 2];
+            for (int i = 0; i < chetNech2H.Length; i++)
+            {
+                if ((i + 1) % 2 == 0)
+                    chetNech2H[i] = fx2h[i + 1] * 2;
+                else chetNech2H[i] = fx2h[i + 1] * 4;
+                resultat += chetNech2H[i] + " , ";
+            }
+            resultat += "} \r\n\r\n";
+
+            //значение h
+            double sumh = 0;
+            for (int i = 0; i < chetNechH.Length - 1; i++)
+            {
+                sumh += chetNechH[i];
+            }
+            sZnachH = h/3 * (fxh[0] +fxh[n - 1] + sumh);
+            resultat += "I(h) = " + sZnachH + "\r\n\r\n";
+
+            //значение 2h
+            double sum2h = 0;
+            for (int i = 0; i < chetNech2H.Length - 1; i++)
+            {
+                sum2h += chetNech2H[i];
+            }
+            sZnach2H = h / 3 * (fx2h[0] + fx2h[n/2 - 1] + sum2h);
+            resultat += "I(h) = " + sZnach2H + "\r\n\r\n";
+
+            resultat += "I(h) - I(2h) = " + (sZnachH - sZnach2H) / 15 + "\r\n";
+            resultat += "Значение интеграла = " + sZnachH + " +- " + (sZnachH - sZnach2H) / 15 + "\r\n\r\n";
+        }
+
+        private async void mathAsync(string s)
+        {
+            temp = Convert.ToDouble(await CSharpScript.EvaluateAsync(s, ScriptOptions.Default.WithImports("System.Math")));
+        }
+
+        private string form(double x, string f)
+        {
+            string res = "";
+            for (int i = 0; i < f.Length; i++)
+            {
+                if (f[i] == 'x') res += x.ToString();
+                else res += f[i];
+            }
+            res = res.Replace(',', '.');
+            return res;
+        }
+    }
+}
